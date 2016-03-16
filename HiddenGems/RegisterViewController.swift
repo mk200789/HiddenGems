f7bce5e785b74c243a6485f3a4943dcb7b1e6940
Karen Melissa Rojas <kameroli@hotmail.com>
2016-03-15 20:30:13 -0400

    test

---
+++ HiddenGems/RegisterViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterViewController.swift
+//  HiddenGems
+//
+//  Created by Melissa Rojas on 3/15/16.
+//  Copyright © 2016 Melissa Rojas. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+//hola
+
+}
---
+++ RestCall.swift
@@ -0,0 +1,9 @@
+//
+//  RestCall.swift
+//  HiddenGems
+//
+//  Created by Melissa Rojas on 3/10/16.
+//  Copyright © 2016 Melissa Rojas. All rights reserved.
+//
+
+import Foundation
