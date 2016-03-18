e901de39900396280818e24eab86cbd661139a25
Your Name Here <kameroli@hotmail.com>
2016-03-17 20:19:24 -0400

    Create user saves into database

--- HiddenGems/RegisterViewController.swift
+++ HiddenGems/RegisterViewController.swift
@@ -11,33 +11,91 @@ import UIKit
 class RegisterViewController: UIViewController {
 
     
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var repeatPassword: UITextField!
     
     @IBOutlet weak var registerBox: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         registerBox.layer.cornerRadius = 10;
+      
 
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     
-    //Adding space
+   
+    @IBAction func registerButton(sender: UIButton) {
+       
+        postCreateUser()
+        
+        self.username.text = ""
+        self.password.text = ""
+        self.email.text = ""
     }
     
-    comitooo
-    */
+    
+    //POST Create User
+    
+    func postCreateUser(){
+        
+        let url = NSURL(string:"http://54.152.30.2/hg/createuser")!
+        let session = NSURLSession.sharedSession()
+        let postParams = ["username":self.username.text!, "password":self.password.text!, "email":self.email.text!] as Dictionary<String, String>
+        
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(postParams, options: NSJSONWritingOptions())
+            print(postParams)
+            
+        }catch{
+            print("JSON serialization failed")
+        }
+        
+        session.dataTaskWithRequest(request, completionHandler: { (data:NSData?, response:NSURLResponse?, error:NSError?) -> Void in
+            guard let realResponse = response as? NSHTTPURLResponse where
+                realResponse.statusCode == 200 else {
+                    print("Not a 200 response")
+                    print(data)
+                    print(response)
+                    print(error)
+                    return
+                    
+            }
+            
+            if let postString = NSString(data: data!, encoding: NSUTF8StringEncoding) as? String {
+                
+                
+                print("POST:" + postString)
+                self.performSelectorOnMainThread("updatePostLabel:", withObject: postString, waitUntilDone: false)
+            }
+        }).resume()
+        
+    }
+    
+    
+    
+    func updatePostLabel(text: String) {
+        print("POST : " + "Successful")
+    }
+
+    
+
+
 
 }
